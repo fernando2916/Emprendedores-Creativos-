9a04fcf34b9ea0c891e7e07b9103bc3c50555fe2
fernando flores <fernando221097@hotmail.com>
2025-04-22 20:49:45 -0600

    navegacion de paginas, con estilos y adaptabilidad a movil, iniciando archivo rutas admin

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,15 +4,16 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Bienvenido</title>
+  <title>@yield('titulo ') Emprendedore Creativos &copy;</title>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   @livewireStyles
   <script src="https://kit.fontawesome.com/255bc8dd2c.js" crossorigin="anonymous"></script>
 </head>
 <body class="bg-slate-300 dark:bg-fondo-100 dark:text-white selection:bg-selec-100 selection:text-white">
   <x-header/>
-  <main class="pt-16"></main>
-  <h2>Hola mundo</h2>
+  <main class="pt-[70px]">
+    @yield('contenido')
+  </main>
   <x-footer/>
   @livewireScripts
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,10 @@
 <x-layouts.app>
-    
+  @section('titulo ')
+    INICIO |
+  @endsection
+  @section('contenido')
+  <div>
+    <h2>Hola</h2>
+  </div>
+  @endsection
 </x-layouts.app>
